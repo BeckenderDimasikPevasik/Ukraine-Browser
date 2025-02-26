a8b693ba4c6f3fcaaa3c75252cc1bf33dac0a1dd
BeckenderDimasikPevasik <gudilindmitro@gmail.com>
2025-02-26 18:15:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Функція для пошуку
+document.getElementById('search-button').addEventListener('click', () => {
+    // Перенаправлення на Google
+    window.location.href = "https://www.google.de/?hl=de";
+});// Функція для пошуку
+document.getElementById('search-button').addEventListener('click', () => {
+    // Перенаправлення на Google
+    window.location.href = "https://www.google.de/?hl=de";
+});
